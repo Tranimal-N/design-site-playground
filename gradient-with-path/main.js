d66a036974a4ccd7c18a7263552b89658413602c
Tran Nguyen <tranviolin@me.com>
2023-02-13 18:05:34 -0800

    modified css

--- gradient-with-path/main.js
+++ gradient-with-path/main.js
@@ -6,25 +6,24 @@ let col = function(x, y, r, g, b) {
   $.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
   $.fillRect(x, y, 1,1);
 }
+
 let R = function(x, y, t) {
-  return( Math.floor(110 + 64 * Math.cos( (x * x - y * y) / 300 + t )) );
+  return( Math.floor(150 + 64 * Math.cos((x * x - y * y) / 300 + t )));
 }
 
 let G = function(x, y, t) {
-  return( Math.floor(50 + 64 * Math.sin( (x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 300 ) ) );
+  return( Math.floor(25 + 64 * Math.sin( (x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 300 )));
 }
 
 let B = function(x, y, t) {
-  return( Math.floor(110 + 64 * Math.sin( 5 * Math.sin(t / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100) ));
+  return( Math.floor(150 + 64 * Math.sin(5 * Math.sin(t / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100)));
 }
 
 let t = 0;
-let x;
-let y;
 
 let run = function() {
-  for(x = 0; x <= 35; x++) {
-    for(y = 0; y <= 35; y++) {
+  for(let x = 0; x <= 35; x++) {
+    for(let y = 0; y <= 35; y++) {
       col(x, y, R(x, y, t), G(x, y, t), B(x, y, t));
     }
   }
