f7d45de144e0049fbee97556713de3d1d36afaf3
Tran Nguyen <tranviolin@me.com>
2023-03-13 21:13:49 -0700

    color theme explorations

---
+++ easy-react-themes/src/components/square/Square.tsx
@@ -0,0 +1,11 @@
+import './square.css'
+
+const Square = () => {
+    return (
+        <div className='square'>
+            <span>This is a square</span>
+        </div>
+    )
+}
+
+export default Square;
